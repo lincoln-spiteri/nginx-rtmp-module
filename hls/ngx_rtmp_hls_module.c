be498e4a8560afa5f2f4fcc5cb6e8397e72f0c42
Lincoln Spiteri <lincoln@ooyala.com>
2016-12-12 17:10:56 +0000

    Set datetime ID3 tag on every fragment in the playlist.

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -584,9 +584,9 @@ ngx_rtmp_hls_write_playlist(ngx_rtmp_session_t *s)
 
     prev_key_id = 0;
 
-    for (i = 0; i < ctx->nfrags; i++) {
+    for (i < ctx->nfrags; i++) {
         f = ngx_rtmp_hls_get_frag(s, i);
-        if (i == 0 && f->datetime && f->datetime->len > 0) {
+        if (f->datetime && f->datetime->len > 0) {
             p = ngx_snprintf(buffer, sizeof(buffer), "#EXT-X-PROGRAM-DATE-TIME:");
             n = ngx_write_fd(fd, buffer, p - buffer);
             if (n < 0) {
